1a5f5b2f9c9b5fe698bc6512e8f51abca16a9fa2
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-01 13:39:05 -0500

    imported peewee

---
+++ notes.py
@@ -0,0 +1,4 @@
+#first after creating your directory and your file is to run {pipenv install psycopg2 then pipenv install} to install peewee in your file
+#importing peewee
+from peewee import *
+from datetime import date 
